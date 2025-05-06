432168863fdb9f08ce6c2967019b4cb7a8b7c96a
Bikesh <bikeshsapkota12@gmail.com>
2025-05-06 08:26:45 +0545

    api request and response handles

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -8,8 +9,15 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private EditText cityInput;
@@ -31,7 +39,9 @@ public class MainActivity extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue(this);
 
         searchButton.setOnClickListener(v ->{
+            Log.d("click","hello");
             String city = cityInput.getText().toString().trim();
+            Log.d( "api: ",city);
             if(!city.isEmpty()){
                 fetchWeatherData(city);
 
@@ -44,8 +54,38 @@ public class MainActivity extends AppCompatActivity {
     private void fetchWeatherData(String city){
 
         String API_KEY = "fd8127b71f05c5fbc057b39c9e4ec6dc";
-        String url ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ API_KEY;
+        String url ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ API_KEY +"&units=metric";
+
+        JsonObjectRequest request =  new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("api",response.toString());
+                        try {
+                            JSONObject main = response.getJSONObject("main");
+                            JSONObject weather = response.getJSONArray("weather").getJSONObject(0);
 
 
+                            double temperature = main.getDouble("temp");
+                            String descriptioin = weather.getString("description");
+
+                            temperatureText.setText(String.format("%.1f°C",temperature) );
+                            descriptionText.setText(descriptioin);
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.e("error: ", error.toString());
+
+                    }
+                });
+        requestQueue.add(request);
     }
 }
